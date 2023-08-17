812a029826cf79f64c0a45e59d790102ba7de3c2
Marcelo Arantes <marceloarantes19@gmail.com>
2023-08-17 16:23:27 -0300

    Iniciando os trabalhos

--- testes/testeTela.py
+++
@@ -1,9 +0,0 @@
-import requests 
-from tkinter import *
-
-janela = Tk()
-janela.title('Mapa da Romênia')
-janela.geometry("500x500")
-canvas = Canvas(janela, width=450, height=450, bg="white")
-canvas.pack()
-janela.mainloop()
